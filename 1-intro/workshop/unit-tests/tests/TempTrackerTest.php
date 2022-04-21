bf2cd07c6b7a5c2864afa8ca69b2cd2b949afeab
nairod49 <78696310+nairod49@users.noreply.github.com>
2022-04-21 15:10:06 +0200

    ajout des test

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -5,7 +5,65 @@ use PHPUnit\Framework\TestCase;
 require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
-    public function test_something() {
-        $this->assertEqual(42, 42);
+    public function test_insert() {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(50);
+        $this->assertEquals(50, $temp_tracker->get_min());
+        $this->assertEquals(50, $temp_tracker->get_max());
+        $this->assertEquals(50, $temp_tracker->get_mean());
+        $this->assertEquals([50], $temp_tracker->get_temps());
     }
+
+    public function test_string(){
+        $this->expectException(TypeError::class);
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert('a');
+       
+    }
+    public function test_value(){
+        $this->expectException(ValueError::class);
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(-1);
+    }
+
+    public function test_min() {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(50);
+        $temp_tracker->insert(10);
+        $temp_tracker->insert(20);
+        $this->assertEquals(10, $temp_tracker->get_min());
+    }
+
+    public function test_max() {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(50);
+        $temp_tracker->insert(10);
+        $temp_tracker->insert(20);
+        $this->assertEquals(50, $temp_tracker->get_max());
+    }
+
+    public function test_mean() {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(50);
+        $temp_tracker->insert(50);
+        $this->assertEquals(50, $temp_tracker->get_mean());
+    }
+
+    
+
+    public function test_temps() {
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(50);
+        $temp_tracker->insert(10);
+        $temp_tracker->insert(20);
+        $this->assertEquals([50, 10, 20], $temp_tracker->get_temps());
+    }
+
+    public function test_no_insert(){
+        $this->expectException(ValueError::class);
+        $temp_tracker = new TempTracker();
+        $temp_tracker->get_mean();
+        
+    }
+
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -6,8 +6,29 @@ require_once('./src/flatten.php');
 
 class FlattenTest extends TestCase
 {
-    public function test_something()
-    {
-        $this->assertEqual(1337, 1337);
+   public function test_with_null()
+   {
+      $this->assertEquals([], flatten([]));
+      $this->assertEquals([], flatten([[[[]]]]));
+   }
+
+ 
+    public function test_egal()
+ {   $a =rand(0,110);
+     $b =rand(0,110);
+     $c =rand(0,110);
+        $this->assertEquals(array( $a,$b,$c ) , flatten([$a,$b,$c]));
     }
+
+    public function test_nested_once(){
+        $this->assertEquals(array(1,2,3,4,5,6,7,8,9,10), flatten([[1,2,3], [4,5,6], [7,8,9], [10]]));
+    }
+
+    
+
+
+
+    
+
+    
 }
