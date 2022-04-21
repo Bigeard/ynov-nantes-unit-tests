d99810c3ba838c552f06a0553fe774328cd63e90
Bigeard <robin.bigeard@gmail.com>
2022-04-21 14:46:52 +0200

    feat() add github action test

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -2,15 +2,18 @@ Feature('Ynov Nantes');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     // Load Ynov Nantes website
+    console.log('Load Ynov Nantes website');
     I.amOnPage('https://www.ynov-nantes.com/');
     I.waitForText('Ynov Nantes', 3);
 
     // Start search
+    console.log('Start search');
     I.click('.search-cta>a');
     I.waitForText('Que recherchez-vous ?', 3);
     I.fillField('s', 'info');
 
     // Wait and see if element `Bachelor Informatique` exist
+    console.log('Wait and see if element `Bachelor Informatique` exist');
     I.wait(5);
     I.see('Bachelor Informatique', '.tile-title');
 });
