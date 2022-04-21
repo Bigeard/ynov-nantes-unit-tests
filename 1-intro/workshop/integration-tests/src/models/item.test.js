a857a874361d2a1b301c81bdf2a8eda904d87146
guillaume8080 <guillaume.gouy@ynov.com>
2022-04-21 20:37:10 +0200

    my first running test

--- 1-intro/workshop/integration-tests/src/models/item.test.js
+++ 1-intro/workshop/integration-tests/src/models/item.test.js
@@ -5,31 +5,42 @@ const Item = require('../models/Item.js');
 const ItemService = require('../services/itemService');
 const {request} = require("express");
 
+
+const mongoose = require('mongoose');
+
 // assertEquals(chainePasseEnVue , chaineLueEnbase);
 /*test('same string in input as recorded', () => {
     expect(true).toBe(true);
 });*/
 
-jest.setTimeout(12000);
+//jest.setTimeout(12000);
 
-test('l acces a la base n est pas assez performant' , () => {
-    const timeToRequest = Item.find({});
+test('creation d un item et presence rn base' , async () => {
+
+
+        // creation d un contexte donc d 'une nouvelle base pour chaque test
+        mongoose.connect('mongodb://mongo:27017/docker-node-mongo-test1', { useNewUrlParser: true })
+            .then(() => console.log('MongoDB Connected'))
+            .catch(err => console.log(err));
+
+    //const itemCreated = await ItemService.createItem("toto");
+    const item = new Item({name: "toto"});
+    await item.save();
+    //const retourOrm = await Item.find({name:"toto"}).exec();
+    const retourOrm = await Item.findOne({name:"toto"}).exec();
+
+    expect(item.name).toBe(retourOrm.name);
+    // expect(item.date).toBe(retourOrm.date);
+
 })
 //
 test('endpoint /get vs extraction from base' , async () => {
+
     //const item = await ItemService.createItem(req.body);
     //const itemFromDataBase = await ItemService.find('id')
 
-    const itemsFromBase = await ItemService.listItems();
-    const response = request.get('/');
-    expect(itemsFromBase).resolves.toBe(response);
 
+    //const response = request.get('/');
 
 })
-/*
-it('CalculateElectionResults : All the system working with an expected Octogon result', () => {
-    const spy = jest.spyOn(tools, 'octogone').mockReturnValueOnce('Clint Eastwood'); //
-    expect(calculateElectionResults(...validSet)).toBe('Clint Eastwood');
-})
-* */
 
