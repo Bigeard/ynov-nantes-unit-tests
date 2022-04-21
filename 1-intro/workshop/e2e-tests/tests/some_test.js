a1240e2dd072234e03dc6fe318541c953c3434fe
Bigeard <robin.bigeard@gmail.com>
2022-04-21 14:29:06 +0200

    feat() add test ynov nantes land page

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -1,5 +1,18 @@
 Feature('Ynov Nantes');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
+    // Load Ynov Nantes website
     I.amOnPage('https://www.ynov-nantes.com/');
+    I.waitForText('Ynov Nantes', 3);
+
+    // Start search
+    I.click('.search-cta>a');
+    I.waitForText('Que recherchez-vous ?', 3);
+    I.fillField('s', 'info');
+
+    // Wait and see if element `Bachelor Informatique` exist
+    I.wait(5);
+    I.see('Bachelor Informatique', '.tile-title');
 });
+
+// Bonus / Send a message to a friend
