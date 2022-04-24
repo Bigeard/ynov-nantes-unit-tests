e9bc82e2f388196a8f8c831ae7005fcae1aaae38
guillaume8080 <guillaume.gouy@ynov.com>
2022-04-24 10:31:40 +0200

    divers tests

--- 1-intro/workshop/integration-tests/src/models/item.test.js
+++ 1-intro/workshop/integration-tests/src/models/item.test.js
@@ -8,39 +8,73 @@ const {request} = require("express");
 
 const mongoose = require('mongoose');
 
-// assertEquals(chainePasseEnVue , chaineLueEnbase);
-/*test('same string in input as recorded', () => {
-    expect(true).toBe(true);
-});*/
+describe('procedure tests d integrations' , () => {
 
-//jest.setTimeout(12000);
+    beforeAll(async()  => {
+        mongoose.connect('mongodb://mongo:27017/docker-node-mongo-test1', { useNewUrlParser: true })
+            .then(() => console.log('MongoDB Connected'))
+            .catch(err => console.log(err));
+        return Item.deleteMany({});
+    })
 
-test('creation d un item et presence rn base' , async () => {
+    afterEach( async () => {
+        return Item.deleteMany({});
+    })
+
+    afterAll(() => {
+        mongoose.connection.close({});
+    })
+
+    test('test bateau' , async () => {
+        expect('vrai').toBe('vrai');
+    })
+
+    test('creation d un item et presence rn base' , async () => {
 
 
-        // creation d un contexte donc d 'une nouvelle base pour chaque test
         mongoose.connect('mongodb://mongo:27017/docker-node-mongo-test1', { useNewUrlParser: true })
             .then(() => console.log('MongoDB Connected'))
             .catch(err => console.log(err));
 
-    //const itemCreated = await ItemService.createItem("toto");
-    const item = new Item({name: "toto"});
-    await item.save();
-    //const retourOrm = await Item.find({name:"toto"}).exec();
-    const retourOrm = await Item.findOne({name:"toto"}).exec();
+        const item = new Item({name: "toto"});
+        await item.save();
+        //const retourOrm = await Item.find({name:"toto"}).exec();
+        const retourOrm = await Item.findOne({name:"toto"}).exec();
 
-    expect(item.name).toBe(retourOrm.name);
-    // expect(item.date).toBe(retourOrm.date);
-
-})
-//
-test('endpoint /get vs extraction from base' , async () => {
-
-    //const item = await ItemService.createItem(req.body);
-    //const itemFromDataBase = await ItemService.find('id')
+        expect(item.name).toBe(retourOrm.name);
 
 
-    //const response = request.get('/');
+    })
+    test('update d un item' , async () => {
+
+
+        const myItem = new Item({name : "toUpdtate"});
+        const updatedItem = await  Item.findOneAndUpdate({name: "toUpdate"} , {name : "updated"} ,() => {});
+
+        const itemUpdated = Item.findOne({name : "updated"});
+
+
+        expect(myItem._id).toBe(itemUpdated._id);
+
+
+    })
+
+    test("mock mongoose " , async () => {
+
+
+
+        jest.spyOn(Item , 'find').mockImplementationOnce(() => Item.findOne());
+        const myItem = new Item ({name: "toto"});
+        const mySecondItem = new Item({name: "toto"});
+        const onlyOneItemSupposed = Item.find({name: "toto"});
+        var myArray = [];
+        myArray.push(onlyOneItemSupposed);
+        expect(1).toBe(myArray.length);
+
+
+
+
+    })
 
 })
 
