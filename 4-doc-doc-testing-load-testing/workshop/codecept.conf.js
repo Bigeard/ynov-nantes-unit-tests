6e32fada92e43580c73545a225186826390f318c
nairod49 <78696310+nairod49@users.noreply.github.com>
2022-04-29 15:58:18 +0200

    fix(4-doc-testing): add test e2e

---
+++ 4-doc-doc-testing-load-testing/workshop/codecept.conf.js
@@ -0,0 +1,25 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+
+const { cwd } = require('process');
+
+setHeadlessWhen(process.env.HEADLESS);
+setCommonPlugins();
+
+exports.config = {
+  tests: './test/**/e2e-test.test.js',
+  output: 'dist',
+//   include: {
+//     I: './steps_file.js'
+//   },
+  helpers: {
+    Playwright: {
+      waitForTimeout: 15000,
+      show: process.env.HEADLESS === 'true' ? false : true,
+      timeout: 15000,
+    },
+    REST:{}
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'e2e-test'
+}
--- 4-doc-doc-testing-load-testing/workshop/test/e2e-test.test.js
+++ 4-doc-doc-testing-load-testing/workshop/test/e2e-test.test.js
@@ -1,10 +1,32 @@
-// To run if codecept is not set:
 
+Feature('Test e2e Todo');
+const rand_string = Math.random().toString(16).substr(2, 8);
 
+Scenario('Test scenario: add Todo in the view', ({ I }) => {
+    // Load page localhost:5000
+    I.amOnPage('http://localhost:5000');
+    I.waitForText('What do I need to do?', 3);
 
-// Feature('add Item to the view');
+    // Add text
+    I.fillField('#newTODO', rand_string);
+    I.click('#create-todo');
 
-// Scenario('La liste s instancie' (I) => {
-//     I.amOnPage("localhost:3000/")
-// });
- 
+    // Wait and see if element `Hello World` exist
+    I.wait(3);
+    I.see(rand_string, '#todo-body');
+});
+
+Scenario('test scenario: done todo', ({ I }) => {
+    // Load page localhost:5000
+    I.amOnPage('http://localhost:5000');
+    I.waitForText('What do I need to do?', 3);
+    I.waitForText(rand_string, 3);
+
+    // Done todo
+    const id = locate('#todo-body tr').last().find('button')
+    I.click(id);
+    
+    // Wait and see if element `Hello World` exist
+    I.wait(3);
+    I.see(rand_string, '#done-body');
+});
